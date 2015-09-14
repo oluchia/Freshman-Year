a7a48379c9e71adac0ea15c8bbd2e634628284b9
Oluchi Anyabuike <OluchiAnyabuike@oluchis-mbp.nuigalway.ie>
2015-09-14 11:11:25 +0100

    Initial Commit

---
+++ lecture01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lecture01
+//
+//  Created by Oluchi Anyabuike on 14/09/2015.
+//  Copyright (c) 2015 Oluchi Anyabuike. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
